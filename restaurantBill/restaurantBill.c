5246f06e39133dfad63eb124906a78b653ab7d28
Mega Sari <misari@cpp.edu>
2021-09-24 14:32:02 -0700

    restaurant bill

--- restaurantBill/restaurantBill.c
+++ restaurantBill/restaurantBill.c
@@ -1,3 +1,11 @@
+/*
+Restaurant Bill Outline :
+1) create a pseudo-random to display the menu listed for the user. using the time and srand from the lecture. 
+2) create lines that ask the user to input the tip and tax percentage using scanf
+3) create a switch statement to display the different menus
+4) create lines that let the user know how much tip, tax, and total they need to pay using printf
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -18,19 +26,19 @@ int main()
     switch(MenuListed)
     {
         case 0:
-            printf("Salad cost : $9.95");
+            printf("Salad cost : $9.95\n");
             price = 9.95;
             break;
         case 1:
-            printf("Soup cost : $4.55");
+            printf("Soup cost : $4.55\n");
             price = 4.55;
             break;
         case 2:
-            printf("Sandwich cost : $13.25");
+            printf("Sandwich cost : $13.25\n");
             price = 13.25;
             break;
         case 3:
-            printf("Pizza cost : $22.35");
+            printf("Pizza cost : $22.35\n");
             price = 22.35;
             break;       
     }
